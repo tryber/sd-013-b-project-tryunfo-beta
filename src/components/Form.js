57f6d63cd16b02c86a76ae3c351945542c628e63
Lucas Caribé <lucasmoc47@gmail.com>
2021-09-30 06:07:47 -0300

    Corrigindo erro no linter

--- src/components/Form.js
+++ src/components/Form.js
@@ -142,7 +142,7 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
