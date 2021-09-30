537f53096ff90a6e57544b3ad8e0d04edbbbeb9f
Joao Victor <joaogmavictor@gmail.com>
2021-09-30 14:54:05 -0300

    Concluindo requisito 2

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 const selectOption = ['normal', 'raro', 'muito raro'];
 
-export default function Form() {
+export default function Form(props) {
+  const {
+    cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage, cardRare,
+    cardTrunfo, hasTrunfo, isSaveButtonDisabled, onInputChange, onSaveButtonClick,
+  } = props;
   return (
     <form className="form-initial">
       <label htmlFor="name">
         Nome da Carta:
-        <input type="text" name="name" id="name" data-testid="name-input" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
+        />
       </label>
       <label htmlFor="descricao">
         Descrição da Carta:
@@ -16,15 +27,19 @@ export default function Form() {
           name="descricao"
           id="descricao"
           data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
         />
       </label>
       <label htmlFor="attr1">
         Attr01:
         <input
-          type="attr1"
+          type="number"
           name="attr1"
           id="descricao"
           data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
         />
       </label>
       <label htmlFor="attr2">
@@ -34,6 +49,8 @@ export default function Form() {
           name="attr2"
           id="attr2"
           data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
         />
       </label>
       <label htmlFor="attr3">
@@ -43,6 +60,8 @@ export default function Form() {
           name="attr3"
           id="attr3"
           data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
         />
       </label>
       <label htmlFor="image">
@@ -52,11 +71,19 @@ export default function Form() {
           name="image"
           id="image"
           data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
         />
       </label>
-      <label htmlFor="image">
+      <label htmlFor="raridade">
         Raridade:
-        <select name="raridade" id="raridade" data-testid="rare-input">
+        <select
+          name="raridade"
+          id="raridade"
+          data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
+        >
           {
             selectOption.map((option) => (
               <option value={ option } key={ option }>{option}</option>
@@ -66,9 +93,24 @@ export default function Form() {
       </label>
       <label htmlFor="trunfo">
         Super Trybe Trunfo:
-        <input type="checkbox" name="trunfo" id="trunfo" data-testid="trunfo-input" />
+        <input
+          type="checkbox"
+          name="trunfo"
+          id="trunfo"
+          data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
+        />
       </label>
-      <button type="button" data-testid="save-button">Salvar</button>
+      <button
+        disabled={ isSaveButtonDisabled }
+        type="button"
+        data-testid="save-button"
+        onClick={ onSaveButtonClick }
+      >
+        Salvar
+
+      </button>
     </form>
   );
 }
