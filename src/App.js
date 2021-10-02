b9d81d6e6ade8c13c8f8284a2576b47f002117ea
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-10-02 11:53:13 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 
+const MIN_ATTR = 0;
+const MAX_ATTR = 90;
+const MAX_POINTS = 210;
 class App extends React.Component {
   constructor() {
     super();
@@ -13,22 +16,60 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   handleChange = ({ target: { name, value, type, checked } }) => {
-    this.setState({ [name]: type === 'checkbox' ? checked : value });
+    this.setState({
+      [name]: type === 'checkbox' ? checked : value,
+    }, () => this.validateInputs());
+  }
+
+  isInputValid = (input) => !!input;
+
+  setRangeBetween0to90 = (attr) => (Number(attr) >= MIN_ATTR && Number(attr) <= MAX_ATTR);
+
+  setMaxPoints = (attr1, attr2, attr3) => (
+    Number(attr1) + Number(attr2) + Number(attr3) <= MAX_POINTS
+  );
+
+  validateInputs = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    const { isInputValid, setRangeBetween0to90, setMaxPoints } = this;
+    const setDisabled = (
+      isInputValid(cardName)
+      && isInputValid(cardDescription)
+      && setRangeBetween0to90(cardAttr1)
+      && setRangeBetween0to90(cardAttr2)
+      && setRangeBetween0to90(cardAttr3)
+      && isInputValid(cardImage)
+      && isInputValid(cardRare)
+      && setMaxPoints(cardAttr1, cardAttr2, cardAttr3)
+    );
+
+    this.setState({ isSaveButtonDisabled: !setDisabled });
   }
 
   render() {
     const defaultProps = this.state;
     return (
       <div>
-        <Form { ...defaultProps } onInputChange={ this.handleChange } />
+        <Form
+          { ...defaultProps }
+          onInputChange={ this.handleChange }
+        />
         <Card { ...defaultProps } />
       </div>
     );
